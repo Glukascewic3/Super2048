f285beae56d9212e803f4ff07f8d63183f916d36
Glukascewic1 Is Back <83483401+Glukascewic3@users.noreply.github.com>
2021-05-01 10:02:44 -0700

    32 - Increment by 2

--- js/game_manager_32.js
+++ js/game_manager_32.js
@@ -72,7 +72,7 @@ GameManager.prototype.addStartTiles = function () {
 // Adds a tile in a random position
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
-    var value = Math.random() < 0.9 ? 1 : 2;
+    var value = Math.random() < 0.9 ? 2 : 4;
     var tile = new Tile(this.grid.randomAvailableCell(), value);
 
     this.grid.insertTile(tile);
@@ -158,7 +158,7 @@ GameManager.prototype.move = function (direction) {
 
         // Only one merger per row traversal?
         if (next && next.value === tile.value && !next.mergedFrom) {
-          var merged = new Tile(positions.next, tile.value + 1);
+          var merged = new Tile(positions.next, tile.value + 2);
           merged.mergedFrom = [tile, next];
 
           self.grid.insertTile(merged);
@@ -170,8 +170,8 @@ GameManager.prototype.move = function (direction) {
           // Update the score
           self.score += merged.value * merged.value;
 
-          // The mighty 36 tile
-          if (merged.value === 36) self.won = true;
+          // The mighty 32 tile
+          if (merged.value === 32) self.won = true;
         } else {
           self.moveTile(tile, positions.farthest);
         }
